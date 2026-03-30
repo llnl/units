b29d9fbd0b170319f778abc553fd4ccc1d56d4bb
Philip Top <phlptp@gmail.com>
2026-03-30 05:41:53 -0700

    fix the license text on module tests

--- test/pkg_test_code/main.cpp
+++ test/pkg_test_code/main.cpp
@@ -1,8 +1,8 @@
 /*
-Copyright (c) 2020-2026,
-Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance
-for Sustainable Energy, LLC.  See the top-level NOTICE for additional details.
-All rights reserved. SPDX-License-Identifier: BSD-3-Clause
+Copyright (c) 2019-2026,
+Lawrence Livermore National Security, LLC;
+See the top-level NOTICE for additional details. All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause
 */
 
 #include UNITS_LIB_HEADER
